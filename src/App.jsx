c66f75cba3f64bd7cc6bf192d732a3a0e1d7fbf7
Arjana <arjana99patel@zohomail.in>
2025-12-09 18:26:12 +0530

    by variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import reactlogo from './assets/react.svg'
 
 const App = () => {
-	
+	const user = "Arjana"
+	const designation = "Software & Engineer"
   return (
 	<div>
-		<h4><b>Arjana</b></h4>
-		<p>Software & Engineer</p>
+		<h4><b>{user}</b></h4>
+		<p>{designation}</p>
 	</div>
    	
   )
